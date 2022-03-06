739b4d30c32814c35079b17dfc8a6ca18c3a9927
Marlon Hernández Herrera <hndzmarlon@gmail.com>
2022-03-06 16:44:20 -0500

    Initial commit

---
+++ app/src/main/java/com/cornershop/counterstest/presentation/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.cornershop.counterstest.presentation.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cornershop.counterstest.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
