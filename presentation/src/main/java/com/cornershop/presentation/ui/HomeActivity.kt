73dd4027d530cf7dac92d530e952c2da13583cba
MarlonHndz <hndzmarlon@gmail.com>
2022-03-07 00:22:26 -0500

    UI Code is moved from App Module to Presentation Module

--- presentation/src/main/java/com/cornershop/presentation/ui/HomeActivity.kt
+++ presentation/src/main/java/com/cornershop/presentation/ui/HomeActivity.kt
@@ -1,4 +1,12 @@
 package com.cornershop.presentation.ui
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cornershop.presentation.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
--- presentation/src/main/java/com/cornershop/presentation/ui/WelcomeActivity.kt
+++ presentation/src/main/java/com/cornershop/presentation/ui/WelcomeActivity.kt
@@ -1,4 +1,12 @@
 package com.cornershop.presentation.ui
 
-class WelcomeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cornershop.presentation.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
 }
