329c751443b68263fb8f2f5e2357ec993ccacba0
MarlonHndz <hndzmarlon@gmail.com>
2022-03-12 01:14:42 -0500

    WelcomeActiviy navigation to Home is implemented

--- presentation/src/main/java/com/cornershop/presentation/ui/WelcomeActivity.kt
+++ presentation/src/main/java/com/cornershop/presentation/ui/WelcomeActivity.kt
@@ -1,6 +1,8 @@
 package com.cornershop.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cornershop.presentation.R
 
@@ -8,5 +10,10 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    }
+
+    fun navigateToHome(view: View) {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
